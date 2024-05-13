8e1245a8202b5a0b458f95269222734d40ca5d97
ElliotMonde <elliotphua@gmail.com>
2024-05-13 16:40:52 +0800

    fix order adapter

--- OrderAdapter.java
+++ OrderAdapter.java
@@ -7,10 +7,11 @@ public class OrderAdapter extends CSVReader implements AdapterInterface<Order> {
   public ArrayList<Order> createList(String filename) {
     ArrayList<Order> instrumentList = new ArrayList<>();
     sc = parseCSV(filename);
+    sc.nextLine();
     while (sc.hasNext()) {
-      String[] nextItem = (sc.nextLine()).split(", ");
-      // in csv: time, orderId, instrumentId, quantity, client, price, side
-      // in order: Order(String orderId, LocalTime time, String clientId, String instrumentId, String side, String price, double quantity)
+        String[] nextItem = (sc.nextLine()).split(",");
+        // in csv: time, orderId, instrumentId, quantity, client, price, side
+        // in order: Order(String orderId, LocalTime time, String clientId, String instrumentId, String side, String price, double quantity)
       Order newOrder = new Order(nextItem[1], LocalTime.parse(nextItem[0]), nextItem[4], nextItem[2], nextItem[6], nextItem[5], Double.parseDouble(nextItem[3]))  ;
       instrumentList.add(newOrder);
     }
